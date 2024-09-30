9ed3e0c701a5490c7ef4d9f612e989e975968a6e
andriana-tomova <andriana.tomova@gmail.com>
2024-09-30 11:56:08 -0400

    test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ days = st.slider("Forecast days:", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-# get the temperature/sky data
+# Get the temperature/sky data
 if place:
     filtered_data = get_data(place, days)
     if option == "Temperature":
